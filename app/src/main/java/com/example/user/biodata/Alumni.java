59dd2dc074e0dde322bc6f101c891dfb0b79473d
yulandari <anisayulandari96@gmail.com>
2016-05-26 20:32:22 +0700

    buat upil

--- app/src/main/java/com/example/user/biodata/Alumni.java
+++ app/src/main/java/com/example/user/biodata/Alumni.java
@@ -4,14 +4,44 @@ package com.example.user.biodata;
  * Created by user on 21/05/2016.
  */
 public class Alumni {
-    String nama;
     String npm;
+    String nama;
+    double ipk;
+    String ttl;
+    String email;
+    String telp;
+    String keahlian;
+    String profesi;
+    String pengalaman;
+    String nama_ortu;
+    String alamat;
+    String judul_skripsi;
+    boolean best;
     String foto;
 
-    public Alumni(String nama, String npm, String foto) {
-        this.nama = nama;
+    public Alumni(String npm, String nama, double ipk, String ttl, String email, String telp, String keahlian, String profesi, String pengalaman, String nama_ortu, String alamat, String judul_skripsi, boolean best) {
         this.npm = npm;
-        this.foto = foto;
+        this.nama = nama;
+        this.ipk = ipk;
+        this.ttl = ttl;
+        this.email = email;
+        this.telp = telp;
+        this.keahlian = keahlian;
+        this.profesi = profesi;
+        this.pengalaman = pengalaman;
+        this.nama_ortu = nama_ortu;
+        this.alamat = alamat;
+        this.judul_skripsi = judul_skripsi;
+        this.best = best;
+        this.foto = npm;
+    }
+
+    public String getNpm() {
+        return npm;
+    }
+
+    public void setNpm(String npm) {
+        this.npm = npm;
     }
 
     public String getNama() {
@@ -22,12 +52,92 @@ public class Alumni {
         this.nama = nama;
     }
 
-    public String getNpm() {
-        return npm;
+    public double getIpk() {
+        return ipk;
     }
 
-    public void setNpm(String npm) {
-        this.npm = npm;
+    public void setIpk(double ipk) {
+        this.ipk = ipk;
+    }
+
+    public String getTtl() {
+        return ttl;
+    }
+
+    public void setTtl(String ttl) {
+        this.ttl = ttl;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelp() {
+        return telp;
+    }
+
+    public void setTelp(String telp) {
+        this.telp = telp;
+    }
+
+    public String getKeahlian() {
+        return keahlian;
+    }
+
+    public void setKeahlian(String keahlian) {
+        this.keahlian = keahlian;
+    }
+
+    public String getProfesi() {
+        return profesi;
+    }
+
+    public void setProfesi(String profesi) {
+        this.profesi = profesi;
+    }
+
+    public String getPengalaman() {
+        return pengalaman;
+    }
+
+    public void setPengalaman(String pengalaman) {
+        this.pengalaman = pengalaman;
+    }
+
+    public String getNama_ortu() {
+        return nama_ortu;
+    }
+
+    public void setNama_ortu(String nama_ortu) {
+        this.nama_ortu = nama_ortu;
+    }
+
+    public String getAlamat() {
+        return alamat;
+    }
+
+    public void setAlamat(String alamat) {
+        this.alamat = alamat;
+    }
+
+    public String getJudul_skripsi() {
+        return judul_skripsi;
+    }
+
+    public void setJudul_skripsi(String judul_skripsi) {
+        this.judul_skripsi = judul_skripsi;
+    }
+
+    public boolean isBest() {
+        return best;
+    }
+
+    public void setBest(boolean best) {
+        this.best = best;
     }
 
     public String getFoto() {
---
+++ app/src/main/java/com/example/user/biodata/DatabaseHelper.java
@@ -0,0 +1,74 @@
+package com.example.user.biodata;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+
+import com.readystatesoftware.sqliteasset.SQLiteAssetHelper;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by My Computer on 5/26/2016.
+ */
+public class DatabaseHelper extends SQLiteAssetHelper{
+
+    private static final String DATABASE_NAME = "alumni.db";
+    private static final int DATABASE_VERSION = 1;
+    private SQLiteDatabase db;
+
+    private static final String TABLE_Alumni = "alumni";
+
+    private static final String COL_NPM = "npm";
+    private static final String COL_NAMA = "nama";
+    private static final String COL_IPK = "ipk";
+    private static final String COL_TTL = "ttl";
+    private static final String COL_EMAIL = "email";
+    private static final String COL_TELP = "telp";
+    private static final String COL_KEAHLIAN = "keahlian";
+    private static final String COL_PROFESI = "profesi";
+    private static final String COL_PENGALAMAN = "pengalaman";
+    private static final String COL_NAMA_ORTU = "nama ortu";
+    private static final String COL_ALAMAT = "alamat";
+    private static final String COL_JUDUL_SKRIPSI = "judul skripsi";
+    private static final String COL_BEST = "best";
+
+
+    public DatabaseHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+
+    }
+
+    public List<Alumni> getAllAlumni() {
+        List<Alumni> Alumni = new ArrayList<>();
+
+        String query = "SELECT * FROM Alumni";
+
+        db = this.getReadableDatabase();
+
+        Cursor c = db.rawQuery(query, null);
+
+        if (c.moveToFirst()){
+            do {
+               Alumni.add(new Alumni(
+                       c.getString(c.getColumnIndex(COL_NPM)),
+                       c.getString(c.getColumnIndex(COL_NAMA)),
+                       c.getDouble(c.getColumnIndex(COL_IPK)),
+                       c.getString(c.getColumnIndex(COL_TTL)),
+                       c.getString(c.getColumnIndex(COL_EMAIL)),
+                       c.getString(c.getColumnIndex(COL_TELP)),
+                       c.getString(c.getColumnIndex(COL_KEAHLIAN)),
+                       c.getString(c.getColumnIndex(COL_PROFESI)),
+                       c.getString(c.getColumnIndex(COL_PENGALAMAN)),
+                       c.getString(c.getColumnIndex(COL_NAMA_ORTU)),
+                       c.getString(c.getColumnIndex(COL_ALAMAT)),
+                       c.getString(c.getColumnIndex(COL_JUDUL_SKRIPSI)),
+                       c.getInt(c.getColumnIndex(COL_BEST)) == 1 ? true : false
+               ));
+            } while (c.moveToNext());
+        }
+        return Alumni;
+    }
+
+}
